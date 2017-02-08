5da42825c3828303683850a70e952ad5e9234250
Damien <damien.romito@gmail.com>
2017-02-08 11:29:11 -0300

    Initial Commit

---
+++ CollectionViewCustom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewCustom
+//
+//  Created by Damien Romito on 08/02/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
